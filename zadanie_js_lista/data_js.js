d2843ae1516fb9cbcbe8c07736a956797decd1f1
Bartek <bartoszwiadernylodz@gmail.com>
2024-04-17 19:25:03 +0200

    naprawione wstawianie

--- zadanie_js_lista/data_js.js
+++ zadanie_js_lista/data_js.js
@@ -1,20 +1,26 @@
 let listTodo = [
     {
         title: 'Wizyta u lekarza',
-        start_date: '2022-04-22',
+        start_date: '2023-04-22',
         status: 'active',
         prior: 1
     },
     {
         title: 'Przygotowanie oferty dla klienta',
-        start_date: '2022-05-33',
+        start_date: '2024-05-33',
         status: 'active',
         prior: 1
     },
     {
         title: 'Wymiana opon',
-        start_date: '2022-04-29',
-        status: 'active',
+        start_date: '2024-04-29',
+        status: 'inactive',
+        prior: 1
+    },
+    {
+        title: 'spacer',
+        start_date: '2024-12-29',
+        status: 'inactive',
         prior: 1
     }
 ]
--- zadanie_js_lista/kod_js.js
+++ zadanie_js_lista/kod_js.js
@@ -1,14 +1,45 @@
-function creat_li(todo){
-    let ul = document.querySelector("ul.list-group")
-    let li = document.createElement('li')
-    ul.appendChild(li)
-    li.classList.add("list-group-item")
-    li.innerHTML=todo
-    console.info(ul)
+function creat_li(todo) {
+    let ul = document.querySelector("ul.list-group");
+    let li = document.createElement('li');
+    
+    let divrow = document.createElement("div");
+    divrow.classList.add("row");
+   
+    let divstatus = document.createElement("div");
+    divstatus.classList.add("col-2");
+ 
+    let badge = document.createElement("span");
+    badge.classList.add("badge");
+    badge.classList.add("badge-primary");
+    if(todo.status=='active'){
+        badge.style.backgroundColor="green";
+    }else{
+        badge.style.backgroundColor="red";
+    }
+    badge.innerHTML = todo.status;
+   
+    let divtitle = document.createElement("div");
+    divtitle.classList.add("col-7");
+    divtitle.innerHTML = todo.title;
+   
+    let divdate = document.createElement("div");
+    divdate.classList.add("col-3");
+    divdate.innerHTML = todo.start_date;
+   
+    divstatus.appendChild(badge);
+    divrow.appendChild(divtitle);
+    divrow.appendChild(divdate);
+    divrow.appendChild(divstatus);
+   
+    li.classList.add("list-group-item");
+    li.appendChild(divrow);
+   
+    ul.appendChild(li);
 }
-listTodo.forEach(todo => {
-   console.info(todo) 
-   creat_li(todo.title+"->"+todo.start_date+"->"+todo.status+"->"+todo.prior)
-});
 
+listTodo.forEach(todo => {
+   
+   creat_li(todo)
+});
+ 
 creat_li()
