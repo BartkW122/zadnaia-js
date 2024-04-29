f9024ca130649ef5318d33c3dbac639c87df1cbf
Bartek <bartoszwiadernylodz@gmail.com>
2024-04-29 15:34:47 +0200

    usuwanie

--- zadanie_js_lista/data_js.js
+++ zadanie_js_lista/data_js.js
@@ -1,26 +1,33 @@
+let todoStatus = {
+	active: 'Aktywne',
+	done: 'Zrobione',
+	reject: 'Odrzucone'
+}
+
+
 let listTodo = [
-    {
-        title: 'Wizyta u lekarza',
-        start_date: '2023-04-22',
-        status: 'active',
-        prior: 1
-    },
-    {
-        title: 'Przygotowanie oferty dla klienta',
-        start_date: '2024-05-33',
-        status: 'active',
-        prior: 1
-    },
-    {
-        title: 'Wymiana opon',
-        start_date: '2024-04-29',
-        status: 'inactive',
-        prior: 1
-    },
-    {
-        title: 'spacer',
-        start_date: '2024-12-29',
-        status: 'inactive',
-        prior: 1
-    }
+	{
+		title: 'Wizyta u lekarza',
+		stop_date: new Date('2022-04-22 00:00:00'),
+		status: todoStatus.active,
+		prior: 1
+	},
+	{
+		title: 'Przygotowanie oferty dla klienta',
+		stop_date: new Date('2022-05-30 00:00:00'),
+		status: todoStatus.reject,
+		prior: 1
+	},
+	{
+		title: 'Wymiana opon',
+		stop_date: new Date('2024-04-18 00:00:00'),
+		status: todoStatus.done,
+		prior: 1
+	},
+	{
+		title: 'Praca klasowa z JS',
+		stop_date: new Date('2024-04-25 00:00:00'),
+		status: todoStatus.active,
+		prior: 1
+	}
 ]
